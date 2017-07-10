531a434b528de72d7f8ce094cec176807e943b67
Sara Damian <sara.dse@gmail.com>
2017-07-10 19:37:23 +0200

    added style to the page

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const listItems = [
-  {name: 'Home', link:'home.html'},
+  {name: 'Home', link:'index.html'},
   {name: 'Foo', link:'foo.html'},
   {name: 'Bar', link:'bar.html'},
   {name: 'The Rolling Stones', link:'the-rolling-stones.html'},
@@ -12,7 +12,7 @@ const listItems = [
 const list = document.querySelector(".list-item");
 let content = "";
 for (const listItem of listItems){
-  content += `<li><a href="${listItem.link}">
+  content += `<li><a class="link-a" href="${listItem.link}">
   ${listItem.name}
   </a></li>`;
 }
